3da4f620574274d7127cd16bbce0351ded58ee92
DanSViana <sousadaniel208@gmail.com>
2024-08-09 09:02:02 -0300

    Versão 1.0

---
+++ main.py
@@ -0,0 +1,10 @@
+'''
+Crie um programa que receba notas de avaliações em uma quantidade definida pelo usuário, calcula a média e exibe o resultado. Ao terminar envie para o repositório do GitHub e poste o link.
+'''
+notas = [7.7, 9.3, 8.1, 5.4, 6.5]
+
+soma = sum(notas)
+
+media = soma / len(notas)
+
+print("A média das notas é:", media)
